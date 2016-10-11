984249fd7efef544fab3b0b21c4d34322f2448af
AustinRoy7 <royomosh@gmail.com>
2016-10-11 17:29:48 +0300

    Changed API from urllib2 to requests

--- Day 2/uni_search_api.py
+++ Day 2/uni_search_api.py
@@ -2,15 +2,16 @@
 
 #Program that searches for  universities in a country and returns the country, name and domain of the university utilising the hipolabs university API
 
-import urllib2
+import requests
 import json
 
 def uni_search(query):
     url = 'http://universities.hipolabs.com/search?country='
     country = query.replace(' ', '%20')
     final_url = url + country
-    json_obj = urllib2.urlopen(final_url)
-    data = json.load(json_obj)
+    json_obj = requests.get(final_url)
+    data = json_obj.json()
     
-    for item in data['objects']:
-        print item['country'], item['name'],item['domain']
+    for item in data:
+        for uni in item:
+        	print (uni , " : ", item[uni],)
